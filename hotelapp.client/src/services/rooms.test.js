32720a0abe3864210583456a7131d55900c78016
Joonas Heikura <joonas.heikura@protonmail.com>
2024-11-15 09:14:56 +0000

    tests for services & updated AddRoom tests

--- hotelapp.client/src/services/rooms.test.js
+++ hotelapp.client/src/services/rooms.test.js
@@ -10,8 +10,7 @@ describe('Rooms Service', () => {
     axios.get.mockReset()
   })
 
-  describe('getAll', () => {
-    test('get all rooms', async () => {
+  test('get all rooms', async () => {
       const roomsMock = testData
 
       axios.get.mockResolvedValue({
@@ -22,6 +21,34 @@ describe('Rooms Service', () => {
 
       expect(axios.get).toHaveBeenCalledWith('/api/rooms')
       expect(rooms).toStrictEqual(roomsMock)
-    })
   })
+
+  test('get one room', async () => {
+    const roomMock = testData[0]
+
+    axios.get.mockResolvedValue({
+      data: roomMock,
+    })
+
+    const rooms = await roomService.getById(testData[0]._id)
+
+    expect(axios.get).toHaveBeenCalledWith(`/api/rooms/${testData[0]._id}`)
+    expect(rooms).toStrictEqual(roomMock)
+  })
+
+  test('post new room', async () => {
+    const newRoomMock = testData[0]
+
+    axios.post.mockResolvedValue({
+      data: newRoomMock,
+    })
+
+    delete newRoomMock._id
+
+    const newRoom = await roomService.create(newRoomMock)
+
+    expect(axios.post).toHaveBeenCalledWith('/api/rooms', newRoomMock)
+    expect(newRoom).toStrictEqual(newRoomMock)
+  })
+
 })
